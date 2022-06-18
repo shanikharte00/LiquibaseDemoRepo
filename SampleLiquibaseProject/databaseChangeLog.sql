33c398786075040b97cbd109217fdb648523c73a
Administrator <Administrator@172.31.86.117>
2022-06-18 11:52:54 +0000

    added tables

--- SampleLiquibaseProject/databaseChangeLog.sql
+++ SampleLiquibaseProject/databaseChangeLog.sql
@@ -18,3 +18,25 @@ VALUES(1, 'Koyale', 'Mayur', 'xyz', 'Mumbai');
 
 
 --rollback DELETE from Persons 
+
+--changeset DDL:2
+CREATE TABLE Student (
+    RollNo int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+--rollback drop table Student
+
+--changeset DDL:3
+CREATE TABLE Employee (
+    EmpId int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+--rollback drop table Employee
